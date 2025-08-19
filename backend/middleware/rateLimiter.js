4ad1ed36ac377776a743731350d72ff6837582f9
Yashindi Bhagya <yashindibhagya@gmail.com>
2025-08-19 17:51:58 +0530

    Add transaction routes and x64 core library for enhanced functionality
    - Implemented transaction retrieval, creation, and deletion routes in transactionRoutes.js.
    - Added summary endpoint to calculate user transaction balance, income, and expenses.
    - Introduced x64 core library for improved cryptographic operations.

---
+++ backend/middleware/rateLimiter.js
@@ -0,0 +1,23 @@
+import ratelimit from "../config/upstash";
+
+const ratelimiter = async (req, res, next) => {
+    try {
+        // here we just kept it simple
+        // in a real-world-app you'd like to put the userId or ipAddress as your key
+        const { success } = await ratelimit.limit("my-rate-limit")
+
+        if (!success) {
+            return res.status(429).json({
+                message: "Too many requests, please try again later."
+            });
+        }
+
+        next()
+
+    } catch (error) {
+        console.log("Rate limit error", error);
+        next(error);
+    }
+}
+
+export default ratelimiter;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 import express from "express"
 import dotenv from "dotenv"
 import { sql } from "./config/db.js";
+import ratelimiter from "./middleware/rateLimiter.js";
+import transactionRoute from "./routes/transactionRoutes.js"
 
 dotenv.config();
 
 const app = express();
 
 //middleware
+app.use(ratelimiter);
 app.use(express.json());
 
 const PORT = process.env.PORT || 5001;
@@ -29,69 +32,11 @@ async function initDB() {
     }
 }
 
-app.get("/api/transactions/:userId", async (req, res) => {
-    try {
-        const { userId } = req.params;
+app.get("/", (req, res) => {
+    res.send("it's working")
+});
 
-        const transactions = await sql`
-        SELECT * FROM transactions WHERE user_id = ${userId} ORDER BY created_at DESC
-        `
-
-        res.status(200).json(transactions);
-    } catch (error) {
-        console.log("Error gettnig the transactions", error)
-        res.status(500).json({ message: "Internal server error" })
-    }
-})
-
-app.post("/api/transactions", async (req, res) => {
-    try {
-        const { title, amount, category, user_id } = req.body;
-
-        if (!title || !user_id || !category || amount === undefined) {
-            return res.status(400).json({ message: "All fields are required" });
-        }
-
-        const transaction = await sql`
-        INSERT INTO transactions(user_it, title, amount, category
-        VALUES (${user_id},${title},${amount},${category})
-        RETURNING *
-        `
-
-        console.log(transaction);
-        res.status(201).json(transaction[0])
-
-
-    } catch (error) {
-        console.log("Error creating the transaction", error)
-        res.status(500).json({ message: "Internal server error" })
-    }
-})
-
-app.delete("/api/transactions/:id", async (req, res) => {
-    try {
-        const { id } = req.params;
-
-        if (isNaN(parseInt(id))) {
-            return res.status(400).json({ message: "Invalid transaction ID" })
-        }
-
-        const result = await sql`
-        DELETE FROM transactions WHERE id = ${id} RETURNING * 
-        `
-
-        if (result.length === 0) {
-            return res.status(404).json({ message: "Transaction not found" })
-        }
-
-        res.status(200).json({ message: "Transaction deleted successfully" })
-
-    } catch (error) {
-        console.log("Error deleting the transaction", error)
-        res.status(500).json({ message: "Internal server error" })
-    }
-})
-
+app.use("/api/transactions", transactionRoute)
 initDB().then(() => {
     app.listen(PORT, () => {
         console.log("Server is up and running port:", PORT)
